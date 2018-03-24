9922f9fc5cc797a05dd3b485e1e72026b7d869c4
Ahmed Elhendawy <ahm_elhen@yahoo.com>
2018-03-24 14:27:48 +0200

    ToDo Model class for encapsulate fields

---
+++ app/src/main/java/com/example/android/todolist/Model/ToDo.java
@@ -0,0 +1,42 @@
+package com.example.android.todolist.Model;
+
+/**
+ * Created by Ahmed El Hendawy on 2018/03/24.
+ */
+
+public class ToDo {
+	private String id, title, description;
+	
+	public ToDo() {
+	}
+	
+	public ToDo(String id, String title, String description) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
